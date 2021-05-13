957dde90d4427169a401bb2574ce90d04770b509
Jimit Doshi <Jimit.Doshi@pennmedicine.upenn.edu>
2021-05-13 10:54:29 -0400

    Added support for using multiple GPUs. Changes include: In get_iou and get_dice, Changed boolean_mask to ragged.boolean_mask to enable returning losses per example. All returned losses now have the [b] instead of []. Dice/IoU losses are powered instead of the metrics themselves. Changed gamma from 0 to 1. Added a loss_weight parameter, alpha, to weight the IoU loss.

--- DeepMRSeg/losses.py
+++ DeepMRSeg/losses.py
@@ -32,9 +32,9 @@ def get_iou( y_true,y_pred ):
 
 	# Shape: [b,c]
 	iou = ( tp ) / ( tp + fp + fn )
-	
+
 	# Mask out NaNs i.e. mask out classes that are not present in y_true as well as in y_pred
-	masked_iou = _tf.boolean_mask( iou, _tf.logical_not( _tf.math.is_nan(iou) ) )
+	masked_iou = _tf.ragged.boolean_mask( iou, _tf.logical_not( _tf.math.is_nan(iou) ) )
 
 	# Shape: [b,c]
 	return masked_iou
@@ -50,7 +50,7 @@ def get_dice( y_true,y_pred ):
 	dice = ( 2*tp ) / ( 2*tp + fp + fn )
 
 	# Mask out NaNs i.e. mask out classes that are not present in y_true as well as in y_pred
-	masked_dice = _tf.boolean_mask( dice, _tf.logical_not( _tf.math.is_nan(dice) ) )
+	masked_dice = _tf.ragged.boolean_mask( dice, _tf.logical_not( _tf.math.is_nan(dice) ) )
 
 	# Shape: [b,c]
 	return masked_dice
@@ -59,92 +59,75 @@ def get_dice( y_true,y_pred ):
 #DEF
 #@_tf.function
 def SoftIOULoss( y_true, y_pred ):
-	
-	iouloss = 1.0 - get_iou( y_true,y_pred )
-
-	# Shape: []
-	return _tf.math.reduce_mean( iouloss )
+	# Shape: [b]
+	return _tf.math.reduce_mean( 1.0 - get_iou( y_true,y_pred ), axis=1 )
 #ENDDEF
 
 #DEF
 #@_tf.function
-def FocalIOULoss( y_true, y_pred, gamma=0 ):
-	
-	focaliouloss = 1.0 - _tf.math.pow( get_iou( y_true,y_pred ),gamma+1 )
-
-	# Shape: []
-	return _tf.math.reduce_mean( focaliouloss )
+def FocalIOULoss( y_true, y_pred, gamma=1 ):
+	# Shape: [b]
+	return _tf.math.reduce_mean( _tf.math.pow( 1.0-get_iou( y_true,y_pred ),gamma ), axis=1 )
 #ENDDEF
 
 #DEF
 #@_tf.function
 def SoftDiceLoss( y_true, y_pred ):
-
-	diceloss = 1.0 - get_dice( y_true,y_pred )
-
-	# Shape: []
-	return _tf.math.reduce_mean( diceloss )
+	# Shape: [b]
+	return _tf.math.reduce_mean( 1.0 - get_dice( y_true,y_pred ), axis=1 )
 #ENDDEF
 
 #DEF
 #@_tf.function
-def FocalSoftDiceLoss( y_true, y_pred, gamma=0 ):
-
-	focaldiceloss = 1.0 - _tf.math.pow( get_dice( y_true,y_pred ),gamma+1 )
-
-	# Shape: []
-	return _tf.math.reduce_mean( focaldiceloss )
+def FocalDiceLoss( y_true, y_pred, gamma=1 ):
+	# Shape: [b]
+	return _tf.math.reduce_mean( _tf.math.pow( 1.0-get_dice( y_true,y_pred ),gamma ), axis=1 )
 #ENDDEF
-
 
 #DEF
 #@_tf.function
-def MAE( y_true, y_pred, gamma=0 ):
-
-	mae = _tf.math.abs( y_true-y_pred )
-
-	# Shape: []
-	return _tf.math.reduce_mean( _tf.math.pow( mae,gamma+1 ) )
+def MAE( y_true, y_pred, gamma=1 ):
+	# Shape: [b]
+	return _tf.math.reduce_mean( _tf.math.pow( _tf.math.abs( y_true-y_pred ),gamma ), axis=[1,2,3] )
 #ENDDEF
-
 
 #DEF
 #@_tf.function
-def BCE( y_true, y_pred, gamma=0 ):
+def BCE( y_true, y_pred, gamma=1 ):
 
 	bce = -1.0 * ( y_true*_tf.math.log( EPS + y_pred ) \
 		+ (1.0-y_true)*_tf.math.log( EPS + 1.0-y_pred ) )
-	
-	# Shape: []
-	return _tf.math.reduce_mean( _tf.math.pow( bce,gamma+1 ) )
+
+	# Shape: [b]
+	return _tf.math.reduce_mean( _tf.math.pow( bce,gamma ),axis=[1,2,3] )
 #ENDDEF
 
 #DEF
 #@_tf.function
-def CombinedLoss( oh,probs,gamma ):
+def CombinedLoss( oh,probs,gamma,alpha ):
 
-	iou_mean = FocalSoftDiceLoss( oh,probs,gamma )
-	mae_mean = MAE( oh,probs,gamma )
-	bce_mean = BCE( oh,probs,gamma )
+	iou = FocalIOULoss( oh,probs,gamma )
+	mae = MAE( oh,probs,gamma )
+	bce = BCE( oh,probs,gamma )
 
-	total_loss_mean = iou_mean + mae_mean + bce_mean
+	total_loss = alpha * iou + (100-alpha) * (mae + bce)
 
-	return total_loss_mean, iou_mean, mae_mean, bce_mean
+	return total_loss, iou, mae, bce
 #ENDDEF
 
 #DEF
 #@_tf.function
-def getCombinedLoss( oh_d1,probs_d1,probs_d2,probs_d4,gamma,ds,xy ):
+def getCombinedLoss( oh_d1,probs_d1,probs_d2,probs_d4,gamma,ds,xy,alpha ):
 
 	oh_d2 = _tf.image.resize( oh_d1,[int(xy/2),int(xy/2)] )
 	oh_d4 = _tf.image.resize( oh_d1,[int(xy/4),int(xy/4)] )
 
-	total_loss_d1, iou_d1, mae_d1, bce_d1 = CombinedLoss( oh_d1,probs_d1,gamma )
+	total_loss_d1, iou_d1, mae_d1, bce_d1 = CombinedLoss( oh_d1,probs_d1,gamma,alpha )
 
 	# IF
 	if ds:
-		total_loss_d2, iou_d2, mae_d2, bce_d2 = CombinedLoss( oh_d2,probs_d2,gamma )
-		total_loss_d4, iou_d4, mae_d4, bce_d4 = CombinedLoss( oh_d4,probs_d4,gamma )
+		total_loss_d2, iou_d2, mae_d2, bce_d2 = CombinedLoss( oh_d2,probs_d2,gamma,alpha )
+		total_loss_d4, iou_d4, mae_d4, bce_d4 = CombinedLoss( oh_d4,probs_d4,gamma,alpha )
 
 		total_loss = total_loss_d1 + 0.5*total_loss_d2 + 0.25*total_loss_d4
 	else:
