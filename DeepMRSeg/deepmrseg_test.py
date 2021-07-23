ba5aa89bf7580d43b6528142aff90f94629afa9c
Guray Erus <gurayerus@gmail.com>
2021-07-23 19:11:27 -0400

    Update test script args

--- DeepMRSeg/deepmrseg_test.py
+++ DeepMRSeg/deepmrseg_test.py
@@ -32,95 +32,116 @@ def read_flags(argv):
 	### Import modules
 	import argparse as _argparse
 
-	parser = _argparse.ArgumentParser( formatter_class=_argparse.ArgumentDefaultsHelpFormatter )
+	exeName = _os.path.basename(argv[0])
 
-#	CONFIG FILE
-#	===========
-	configArgs = parser.add_argument_group( 'CONFIG FILE' )
-	configArgs.add_argument( "--config", default=None, type=str, \
-				help="absolute path to the config file containing the parameters in a JSON format")
+	descTxt = '{prog} applies pre-trained segmentation model(s) on image(s) from single or multiple subjects.'.format(prog=exeName)
 
-	# Only parse for "--config"
-	configarg,_ = parser.parse_known_args()
-
-#	I/O
-#	==========
-	inputArgs = parser.add_argument_group( 'INPUT SCANS - REQUIRED', 'Select one of the 3 options: Single case (inImg/outImg), process all scans in a folder (inDir/outDir/outSuff) or using a scan list (sList)')
+	epilogTxt = '''Examples:
 	
-	## I/O Option1: Single scan I/O
-	inputArgs.add_argument( "--inImg", action='append', default=None, type=str, \
-				help="I/O Option1: Input scan name(s)")
-	inputArgs.add_argument( "--outImg", default=None, type=str, \
-				help="I/O Option1: Output scan name")
+  ## Apply single-modal segmentation task on single image (I/O OPTION 1)
+  {prog} -m /my/models/bmask_mdldir --inImg sub1_T1.nii.gz --outImg sub1_bmaskseg.nii.gz
+ 
+  ## Apply multi-modal (FL and T1) segmentation task on single subject (I/O OPTION 1)
+  {prog} -m /my/models/wmlesion_mdldir --inImg sub1_FL.nii.gz --inImg sub1_T1.nii.gz --outImg sub1_wmlseg.nii.gz
 
-	## I/O Option2: Batch I/O from folder
-	inputArgs.add_argument( "--inDir", default=None, type=str, \
-				help="I/O Option2: Input folder name")
-	inputArgs.add_argument( "--outDir", default=None, type=str, \
-				help="I/O Option2: Output folder name")
-	inputArgs.add_argument( "--outSuff", default='_SEG', type=str, \
-				help="I/O Option2: Output suffix")
+  ## Apply multi-modal segmentation task on multiple images using an image list (I/O OPTION 2)
+  {prog} -m /my/models/wmlesion_mdldir --sList my_img_list.csv
+     with my_img_list.csv:
+       ID,FLAIR,T1,OutImg
+       sub1,/my/indir/sub1_FL.nii.gz,/my/indir/sub1_T1.nii.gz,/my/outdir/sub1_wmlseg.nii.gz
+       sub2,/my/indir/sub2_FL.nii.gz,/my/indir/sub2_T1.nii.gz,/my/outdir/sub2_wmlseg.nii.gz
+       ...
+     
+  ## Apply single-modal segmentation task on multiple images within a folder (I/O OPTION 3)
+  {prog} -m /my/models/bmask_mdldir --inDir /my/indir --outDir /my/outdir --inSuff _T1.nii.gz --outSuff _bmaskseg.nii.gz
+ 
+  ## Apply multiple models and combine segmentations
+  {prog} -m /my/models/bmask_mdldir1 -m /my/models/bmask_mdldir2 ... [I/O OPTIONS]
+  
+  '''.format(prog=exeName)
 
-	## I/O Option3: Batch I/O from list
-	inputArgs.add_argument( "--sList", default=None, type=str, \
-				help="I/O Option3: Scan list")
+	parser = _argparse.ArgumentParser( formatter_class=_argparse.RawDescriptionHelpFormatter, \
+		description=descTxt, epilog=epilogTxt )
 
 #	MODELS
 #	===========
-	dirArgs = parser.add_argument_group( 'MODELS - REQUIRED', 'Trained model directories')
+	dirArgs = parser.add_argument_group( 'MODEL', 'Trained model directory')
 	dirArgs.add_argument( "--mdlDir", action='append', default=None, type=str, \
-				help="absolute path to the directory where the model \
-					should be loaded from.")
+		help=	'Path to the directory where the trained model should be loaded from. \
+			Relative or absolute paths can be used. The user can enter multiple models \
+			as "--mdlDir Model1 --mdlDir Model2 ...". In case of multiple models, \
+			each model will be applied individually, and the output of multiple models \
+			will be combined into a single segmentation. (REQUIRED)')
 
-#	INPUT OPTIONS
-#	===========
-	inpArgs = parser.add_argument_group( 'INPUT IMAGE' )
-	inpArgs.add_argument( "--prep", default=False, action="store_true", \
-				help="flag indicating that the input images are \
-					already preprocessed (default: False)")
-
-	inpArgs.add_argument( "--xy_width", default=320, type=int, \
-				help="xy dimensions of the input patches. \
-					Determines how much each slice needs to be padded \
-					in the xy dimension. Should be divisible by 2**depth.")
-
-	inpArgs.add_argument( "--batch", default=64, type=int, \
-				help="batch size" )
-
-#	OUTPUT OPTIONS
-#	==============
-	outArgs = parser.add_argument_group( 'OUTPUT' )
-	outArgs.add_argument( "--probs", default=False, action="store_true", \
-				help="flag to indicate whether the probabilities should \
-					be stored as nii.gz (default: no)")
-
-#	MISC
-#	====
-	miscArgs = parser.add_argument_group( 'MISCELLANEOUS' )
-	miscArgs.add_argument( "--verb", default=False, action="store_true", \
-				help="verbosity")
-	miscArgs.add_argument( "--nJobs", default=None, type=int, \
-				help="number of jobs/threads" )
-	miscArgs.add_argument( "--delay", default=1, type=int, \
-				help="delay between launching parallel prediction \
-					for each subject (default: 1)" )
-	miscArgs.add_argument( "--tmpDir", default=None, type=str, \
-                help="absolute path to the temporary directory" )
+#	I/O
+#	==========
+	## I/O Option1: Single case processing
+	ioArgs1 = parser.add_argument_group( 'I/O OPTION 1', 'Single case processing')
+	ioArgs1.add_argument( "--inImg", action='append', default=None, type=str, \
+		help=	'Input image file name. Relative or absolute paths can be used. \
+			For models that require multi-modal input, the user can enter multiple images \
+			as "--inImg imgMod1 --inImg imgMod2 ...". The number and order of input image \
+			modalities should match the ones used during training. (REQUIRED)')
 	
+	ioArgs1.add_argument( "--outImg", default=None, type=str, \
+		help=	'Output image file name. Relative or absolute paths can be used. \
+			Output image will be saved as a ".nii.gz" image. \
+			The output folder will be created if it does not exist. \
+			Processing is aborted if output image exists. (REQUIRED)')
 
-#	FLAGS
-#	=====
-	### Read config file first, if provided
-	if configarg.config:
-		# Read the JSON config file
-		with open(configarg.config) as f:
-			configflags = _json.load(f)
+	## I/O Option2: Batch I/O from list
+	ioArgs2 = parser.add_argument_group( 'I/O OPTION 2', 'Batch processing using image list')
+	ioArgs2.add_argument( "--sList", default=None, type=str, \
+		help=	'Image list file name. Enter a comma separated list file with columns for: \
+			ID, input image(s) and output image. \
+			Column names can be any text.  \
+			Every row includes input and output image names for one subject. All image names \
+			should be absolute paths. For models that require multi-modal input, \
+			the user can enter input images in multiple columns. The number and order \
+			of input image modalities should match the ones used during training. (REQUIRED)')
 
-		# Set args from the config file as defaults
-		parser.set_defaults( **configflags )
+	## I/O Option3: Batch I/O from folder
+	ioArgs3 = parser.add_argument_group( 'I/O OPTION 3', 'Batch processing of all images in a folder')
+	ioArgs3.add_argument( "--inDir", default=None, type=str, \
+		help=	'Input folder name. All images with the input suffix within the input folder \
+			and its subfolders are processed individually. WARNING: I/O OPTION 3 can not be used \
+			with models that require multi-modal input. For such cases please use I/O OPTION 2! (REQUIRED)')
+	
+	ioArgs3.add_argument( "--outDir", default=None, type=str, \
+		help=	'Output folder name. The output folder will be created if it does not exist. \
+			Output images will be saved as ".nii.gz" images, named as input image file name \
+			appended with the output suffix. (REQUIRED)')
+	
+	ioArgs3.add_argument( "--inSuff", default='.nii.gz', type=str, \
+		help='Input image suffix (default: .nii.gz)')
+	
+	ioArgs3.add_argument( "--outSuff", default='_SEG.nii.gz', type=str, \
+		help="Output image suffix  (default: _SEG.nii.gz)")
 
-	### Read remaining args from CLI and overwrite the defaults
-	flags = parser.parse_args(argv)
+#	OTHER OPTIONS
+#	===========
+	otherArgs = parser.add_argument_group( 'OTHER OPTIONS')
+
+	otherArgs.add_argument( "--batch", default=64, type=int, \
+		help="Batch size  (default: 64)" )
+
+	otherArgs.add_argument( "--probs", default=False, action="store_true", \
+		help=	'Flag to indicate whether to save a probability map for each segmentation label. \
+			Probability maps will be saved with the output image name appended with the \
+			suffix "_prob[N].nii.gz", where [N] is the label for each class type \
+			(example: _prob0.nii.gz: background probability map, _prob1.nii.gz: foreground \
+			probability map, for a binary segmentation task). (default: False)')
+
+	otherArgs.add_argument( "--nJobs", default=None, type=int, \
+		help="Number of jobs/threads (default: automatically determined)" )
+	
+	otherArgs.add_argument( "--tmpDir", default=None, type=str, \
+                help=	'Absolute path to the temporary directory. If not provided, a temporary directory \
+			will be created automatically and will be deleted at the end of processing. \
+			The user defined temporary directory and its contents will not be removed (default: None)' )
+
+	### Read args from CLI
+	flags = parser.parse_args(argv[1:])
 
 	### Return flags and parser
 	return flags, parser
@@ -393,62 +414,56 @@ def _main_warg(argv):
 	_signal.signal( _signal.SIGINT, signal_handler )
 	_signal.signal( _signal.SIGTERM, signal_handler )
 
+	##################################################
 	### Read command line args
-	print("\nParsing args    : %s\n" % (argv[ 1: ]) )
-	FLAGS,parser = read_flags(argv[1:])
-	print(FLAGS)
-
-	_sys.stdout.flush()
-
-	### Check the number of arguments
+	print("\nParsing args    : %s\n" % (argv) )
+	FLAGS,parser = read_flags(argv)
 	if len( argv ) == 1:
 		parser.print_help( _sys.stderr )
 		_sys.exit(1)
+	print(FLAGS)
+	_sys.stdout.flush()
 
 	##################################################
 	### Check required args
 
-	## Detect input type
+	## Check mdlDir
+	if FLAGS.mdlDir is not None:                        ## Mdl dir
+		for tmpPath in FLAGS.mdlDir:
+			pythonUtilities.check_file( tmpPath )
+	else:
+		print('ERROR: Missing required arg: --mdlDir')
+		parser.print_help( _sys.stderr )
+		_sys.exit(1)
+
+	## Check I/O OPTIONS
 	if FLAGS.inImg is not None:                         ## CASE 1: Single inImg
 		IOType = 1
 		for tmpImg in FLAGS.inImg:
 			pythonUtilities.check_file( tmpImg )
 
-	elif FLAGS.inDir is not None:                       ## CASE 2: Img dir
+	elif FLAGS.sList is not None:                       ## CASE 2: Img list
 		IOType = 2
+		pythonUtilities.check_file( FLAGS.sList )
+		
+	elif FLAGS.inDir is not None:                       ## CASE 3: Img dir
+		IOType = 3
 		pythonUtilities.check_file( FLAGS.inDir )
 		
 		if FLAGS.outDir is None:
 			print('ERROR: Missing required arg: outDir')
 			parser.print_help( _sys.stderr )
 			_sys.exit(1)
-
-	elif FLAGS.sList is not None:                       ## CASE 3: Img list
-		IOType = 3
-		pythonUtilities.check_file( FLAGS.sList )
-		
+			
 	else:
-		print('ERROR: Missing required arg - The user should set one of the 3 I/O options (inImg/outImg, inDir/outDir/outSuff or sList)')
+		print('ERROR: Missing required arg - The user should set one of the 3 I/O OPTIONS')
 		parser.print_help( _sys.stderr )
 		_sys.exit(1)
 
-	if FLAGS.mdlDir is not None:                        ## Mdl dir
-		for tmpPath in FLAGS.mdlDir:
-			pythonUtilities.check_file( tmpPath )
-	else:
-		print('ERROR: Missing required arg - The user should set one (or more) model path (mdlDir)')
-		parser.print_help( _sys.stderr )
-		_sys.exit(1)
+	##################################################
+	### Check/set optional args
 
-
-	# Check if xy_width matches the depth
-	# IF
-	if FLAGS.xy_width % 16 != 0:
-		print( "ERROR: The xy_width (%d) is not divisible by %d" % (FLAGS.xy_width,16) )
-		_sys.exit(1)
-	# ENDIF
-	
-	# if nJobs not defined
+	# Set nJobs
 	#IF
 	if FLAGS.nJobs:
 		nJobs = FLAGS.nJobs
@@ -456,7 +471,7 @@ def _main_warg(argv):
 		nJobs = _os.cpu_count()
 	#ENDIF
 
-	# Create temp dir
+	# Set tmpDir
 	#IF
 	if FLAGS.tmpDir:
 		user_defined_tmpdir = True
@@ -467,33 +482,28 @@ def _main_warg(argv):
 		user_defined_tmpdir = False
 		tmpDir = _tempfile.mkdtemp( prefix='deepmrseg_test_' )
 	#ENDIF
-
-	print("Temp folder set to : " + tmpDir)
-	_sys.stdout.flush()
 	
 
+	##################################################
 	### Print parsed args
+	print("\nModel dir(s) \t: %s" % (FLAGS.mdlDir))
 	if IOType == 1:
 		print( "\nInput image(s) \t: %s" % (' , '.join(FLAGS.inImg)) )
 		print( "\nOutput image \t: %s" % (FLAGS.outImg) )
 	if IOType == 2:
+		print( "\nImage List \t: %s" % (FLAGS.sList) )
+	if IOType == 3:
 		print( "\nInput dir \t: %s" % (FLAGS.inDir) )
 		print( "\nOutput dir \t: %s" % (FLAGS.outDir) )
+		print( "\nInput suffix \t: %s" % (FLAGS.outDir) )
 		print( "\nOutput suffix \t: %s" % (FLAGS.outSuff) )
-	if IOType == 3:
-		print( "\nFile List \t: %s" % (FLAGS.sList) )
-
-	print( "Data Prepped \t: %d" % (FLAGS.prep) )
-	
-	print("\nModel Dir(s) \t: %s" % (FLAGS.mdlDir))
-
-	print("Batch Size \t: %s" % (FLAGS.batch))
-	
+	print("\nBatch Size \t: %s" % (FLAGS.batch))
 	print("\nOutput probs \t: %d" % (FLAGS.probs))
+	print("\nNumber of jobs \t: %d" % (nJobs))
+	print("\nTemp folder \t: %s" % (tmpDir))
 	
-	import csv as _csv
-	from concurrent.futures import ThreadPoolExecutor as _TPE
-
+	_sys.stdout.flush()
+	
 	###############################################################
 	#### For direct I/O options: Create list file with in/out images 
 	if IOType == 1:
@@ -521,7 +531,7 @@ def _main_warg(argv):
 		# Set flag for image list
 		FLAGS.sList = outCsv
 
-	if IOType == 2:
+	if IOType == 3:
 
 		# Generate list of columns
 		numMod = 1          ## Multi-modal data is not implemented for "input from folder"
@@ -533,17 +543,21 @@ def _main_warg(argv):
 		FLAGS.inDir = _os.path.abspath(FLAGS.inDir)
 		FLAGS.outDir = _os.path.abspath(FLAGS.outDir)
 		
-		# Add image extension to out suffix (.nii.gz for now)
+		# Add image extension to in/out suffix (.nii.gz for now)
+		if FLAGS.inSuff.endswith('.nii.gz') == False:
+			FLAGS.inSuff = FLAGS.inSuff + '.nii.gz'
+
+		# Add image extension to in/out suffix (.nii.gz for now)
 		if FLAGS.outSuff.endswith('.nii.gz') == False:
 			FLAGS.outSuff = FLAGS.outSuff + '.nii.gz'
 			
 		# Create data frame
-		#   Get list of all .nii.gz images in the input dir
+		#   Get list of all images with inSuff in the input dir
 		data = []
 		subInd = 1
 		for (dir, _, files) in _os.walk(FLAGS.inDir):
 			for i,f in enumerate(files):
-				if f.endswith('.nii.gz'):
+				if f.endswith(FLAGS.inSuff):
 					pathIn = _os.path.join(dir, f)
 					pathOut = _os.path.join(FLAGS.outDir, f).replace('.nii.gz', FLAGS.outSuff)
 					data.append(['Scan' + str(subInd), pathIn, pathOut])
@@ -563,6 +577,8 @@ def _main_warg(argv):
 
 	###############################################################
 	#### Run testing independently for each model path
+
+	from concurrent.futures import ThreadPoolExecutor as _TPE
 
 	num_models = len(FLAGS.mdlDir)
 	FLAGS.mdlDir = list(map(_os.path.abspath, FLAGS.mdlDir))   
@@ -595,14 +611,6 @@ def _main_warg(argv):
 		
 		allmodels = []
 		# Launch threads to load models simultaneously
-		print("")
-
-		## TODO: THIS IS TO PRINT THE HEAP USAGE, WILL BE REMOVED IN RELEASE VERSION
-		#h=hpy()
-		#print('----------- S1 ----------------------------------------')
-		#print(h.heap())
-		#print('--------------------------------------------------------')
-		#_sys.stdout.flush()
 
 		#WITH
 		with _TPE( max_workers=None ) as executor:
@@ -616,22 +624,8 @@ def _main_warg(argv):
 		print("")
 		_sys.stdout.flush()
 		
-		## TODO: THIS IS TO PRINT THE HEAP USAGE, WILL BE REMOVED IN RELEASE VERSION
-		#h=hpy()
-		#print('----------- S2 ----------------------------------------')
-		#print(h.heap())
-		#print('--------------------------------------------------------')
-		#_sys.stdout.flush()
-		
 		_tf.keras.backend.clear_session()
 		
-		## TODO: THIS IS TO PRINT THE HEAP USAGE, WILL BE REMOVED IN RELEASE VERSION
-		#h=hpy()
-		#print('----------- S3 ----------------------------------------')
-		#print(h.heap())
-		#print('--------------------------------------------------------')
-		#_sys.stdout.flush()
-
 		### Encode indices to ROIs if provided
 		roi_indices = get_roi_indices(roicsv=roiIndFile)
 
@@ -658,7 +652,7 @@ def _main_warg(argv):
 				subId = row[idCol]
 				refImg = row[refMod]
 				outImg = row[outCol]
-
+				
 				# Get files for other modalities
 				otherModsFileList = []
 				#IF
@@ -842,4 +836,3 @@ def _main():
 if __name__ == '__main__':
 	_main()
 #ENDIF
-
